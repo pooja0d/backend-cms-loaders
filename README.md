a4f060556c08b440b9e5ec0e1b2fce42eedefe72
Pooja-Dhamanekar259 <Pooja@zerodistance.io>
2021-11-17 00:23:27 +0530

    Revert "Initial commit"
    This reverts commit 133661f0e4e67a858013cb414503e297835ecc12.

--- README.md
+++
@@ -1,2 +0,0 @@
-# backend-cms-loaders
- Loaders
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import logo from "./assets/images/logos/0d.png"; // Logo Icon Asset Import
-import "./App.css"; // App CSS Import
-import MainPage from "./pages/MainPage"; // Main Page Import From Pages Folder of src
-function App() {
-  return <MainPage />;
-}
-
-export default App;
-
